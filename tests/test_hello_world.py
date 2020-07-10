348f0c8e4ba64e573b55db3a2a3255f4cee1c087
Saeid Pahlevan <s.pahlevan@fugro.com>
2020-07-10 15:05:09 +0800

    Modified requirements.txt

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -5,4 +5,4 @@ import hello_world as hello
 def test_solve_complex_equation():
     z = hello.solve_complex_equation(np.array([1, 2]), np.array([3, 7]))
     assert z[0] == 12
-    assert z[1] == 25
+    assert z[1] == 27
